0a653d0a372193ad31f105719b677e5578335ce6
Vivek <vivekmuralidharan.ps@gmail.com>
2018-04-18 08:03:56 -0400

    Changed functional component to class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,11 @@
-import React from 'react';
-const SearchBar = () => {
-  return <input/>;
-};
+import React,{Component} from 'react';
+
+class SearchBar extends Component{
+
+render(){
+      return <input/>;
+  }
+}
+
 
 export default SearchBar;
